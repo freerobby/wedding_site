77cf0f2bce9456746051ce23a64feb04deeb03f0
Robby Grossman <robby@freerobby.com>
2013-09-14 19:38:46 -0400

    More customizations for R-K wedding.

--- constants.rb
+++ constants.rb
@@ -6,7 +6,7 @@ BRIDE_LAST_NAME = 'Coyle'
 GROOM_FIRST_NAME = 'Robby'
 GROOM_LAST_NAME = 'Grossman'
 
-WEDDING_DATE = Date.new(2014, 2, 8)
+WEDDING_DATE = Time.new(2014, 2, 8, 18, 0)
 WEDDING_VENUE = 'Longfellow\'s Wayside Inn'
 WEDDING_STATE = 'Massachusetts'
 WEDDING_ADDRESS = '72 Wayside Inn Rd, Sudbury, MA'
@@ -14,3 +14,6 @@ WEDDING_ADDRESS = '72 Wayside Inn Rd, Sudbury, MA'
 # These get automatically generated based on what's specified above.
 WEDDING_DATE_PRETTY = WEDDING_DATE.strftime('%A, %B %e, %Y')
 WEDDING_DATE_LONGFORM = WEDDING_DATE.strftime("%A, the #{WEDDING_DATE.day.ordinalize} of %B, %Y")
+WEDDING_TIME_PRETTY = WEDDING_DATE.strftime('%l%P')
+
+GOOGLE_ANALYTICS_ID = 'UA-44008076-1'
