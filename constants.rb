e65f22b91d62dc884f6d8c96566212357e1c27d2
Robby Grossman <robby@freerobby.com>
2013-09-14 17:29:06 -0400

    Update wedding details for Robby and Kristin wedding.

--- constants.rb
+++ constants.rb
@@ -1,14 +1,16 @@
 require 'active_support/core_ext/integer/inflections'
 require 'open-uri'
 
-BRIDE_FIRST_NAME = 'Rachel'
-BRIDE_LAST_NAME = 'Zettel'
-GROOM_FIRST_NAME = 'Michael'
-GROOM_LAST_NAME = 'Macasek'
+BRIDE_FIRST_NAME = 'Kristin'
+BRIDE_LAST_NAME = 'Coyle'
+GROOM_FIRST_NAME = 'Robby'
+GROOM_LAST_NAME = 'Grossman'
 
-WEDDING_DATE = Date.new(2014, 5, 25)
-WEDDING_DATE_PRETTY = WEDDING_DATE.strftime('%B %d, %Y')
-WEDDING_DATE_LONGFORM = WEDDING_DATE.strftime("the #{WEDDING_DATE.day.ordinalize} of %B, %Y")
-WEDDING_VENUE = 'Castle Hill on The Crane Estate'
+WEDDING_DATE = Date.new(2014, 2, 8)
+WEDDING_VENUE = 'Longfellow\'s Wayside Inn'
 WEDDING_STATE = 'Massachusetts'
-WEDDING_ADDRESS = 'Castle Hill, 290 Argilla Road, Ipswich, MA 01938'
+WEDDING_ADDRESS = '72 Wayside Inn Rd, Sudbury, MA'
+
+# These get automatically generated based on what's specified above.
+WEDDING_DATE_PRETTY = WEDDING_DATE.strftime('%A, %B %e, %Y')
+WEDDING_DATE_LONGFORM = WEDDING_DATE.strftime("%A, the #{WEDDING_DATE.day.ordinalize} of %B, %Y")
