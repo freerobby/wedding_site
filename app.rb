ce237f9aa0a86e379491203378e4ef74dec39bb7
Robby Grossman <robby@freerobby.com>
2013-10-12 14:01:00 -0400

    Build out complete hotels/travel page.
    Add Amazon registry to wedding site.
    
    Implement RSVPs

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 
 require './constants'
 
+require 'pony'
+
 get '/' do
   @page_title = set_title('Home')
 
@@ -32,6 +34,31 @@ get '/rsvp' do
   erb :rsvp
 end
 
+post '/rsvp' do
+  Pony.mail({
+    to: 'Robby Grossman <robby@freerobby.com>',
+    from: 'Wedding Response <noreply@robbyandkristin.com>',
+    subject: 'Wedding Response Received',
+    body: params_to_email_body(params),
+    via: :smtp,
+    via_options: {
+      address: 'smtp.sendgrid.net',
+      port: '587',
+      user_name: ENV['SENDGRID_USERNAME'],
+      password: ENV['SENDGRID_PASSWORD'],
+      authentication: :plain,
+      domain: 'robbyandkristin.com'
+    }
+  })
+  redirect '/rsvp-success'
+end
+
+get '/rsvp-success' do
+  @page_title = set_title('RSVP Recorded!')
+
+  erb :'rsvp-success'
+end
+
 get '/javascripts/jquery.function.js' do
   erb :'javascripts/jquery.function.js', layout: false
 end
@@ -40,4 +67,10 @@ end
 
 def set_title(page_name)
   "#{GROOM_FIRST_NAME} &amp; #{BRIDE_FIRST_NAME} - #{page_name}"
+end
+
+def params_to_email_body(params)
+  params.inject('') do |result, kv|
+    result += "#{kv[0]}: #{kv[1]}\n"
+  end
 end
